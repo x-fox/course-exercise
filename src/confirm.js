655f6820f04c928192054d952a5e79cc5d301db1
hgx <laoxindeyouxiang@163.com>
2019-10-28 01:14:16 +0800

    modify#方法组件测试

--- src/confirm.js
+++ src/confirm.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from 'react';
+// import React from 'react';
+import ReactDOM from 'react-dom';
 import './confirm.css';
 
-function useIsMounted(){
 
-  // return (
-  //   <div className="confirm">
-  //     <div  ref={(node) => ref = node}>
-  //     </div>
-  //   </div>
-  // )
+// function confirmComp(props) {
+//   return <div>confirm component</div>
+// }
+function Comp(){
   const [isMount, setIsMount] = useState(false);
   useEffect(() => {
     if (!isMount) {
       setIsMount(true);
+      // resolve(true)
     }
     return () => setIsMount(false);
   }, []);
-  return isMount
+  return (<div className="confirm"></div>)
 }
+function confirm(mountedNode=document.body.root) {
+  // const isMounted = confirmComp(props)
 
-function confirm(props) {
-  const isMounted = useIsMounted();
-  return <div>isMounted: {isMounted ? "true" : "false"}</div>;
+  return new Promise((resolve, reject) => {
+    resolve(true)
+    // return ReactDOM.render(<Comp />, mountedNode)
+  });
 }
 export default confirm
 
